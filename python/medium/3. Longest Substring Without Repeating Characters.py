269e49f50bf23d13d9d15f04f2d3c2228c5dc656
Nurkaliyev Dinmukhamed (QypshaQ) <nurkaliyev.dinmukhamed@gmail.com>
2023-01-06 23:34:46 +0600

    Added 9. Palindrome Number

--- python/medium/3. Longest Substring Without Repeating Characters.py
+++ python/medium/3. Longest Substring Without Repeating Characters.py
@@ -1,5 +1,6 @@
 class Solution:
     # 3. Longest Substring Without Repeating Characters
+    # Version 1.
     def lengthOfLongestSubstring(self, s: str) -> int:
         # set to store characters in current substring
         char_set = set()
@@ -22,3 +23,23 @@ class Solution:
                 start += 1
         # return max length
         return max_len
+
+    # Version 2.
+    # def lengthOfLongestSubstring(self, s: str) -> int:
+    #     # dictionary to store characters and their indices in current substring
+    #     char_dict = {}
+    #     # initialize max length
+    #     max_len = 0
+    #     # initialize start of current substring
+    #     start = 0
+    #     # loop through string
+    #     for end, char in enumerate(s):
+    #         # if char already in char_dict, update start to index of char + 1
+    #         if char in char_dict:
+    #             start = max(start, char_dict[char] + 1)
+    #         # update char_dict with char and its index
+    #         char_dict[char] = end
+    #         # update max_len if necessary
+    #         max_len = max(max_len, end - start + 1)
+    #     # return max length
+    #     return max_len
